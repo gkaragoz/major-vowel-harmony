9b6baca664267eb30a4bb60127fbb0dfe9ec61ba
gkaragoz <gkaragoz96@gmail.com>
2020-08-11 21:12:35 +0300

    Add body to isHasMajorVowelHarmony function.

--- major_vowel_harmony.js
+++ major_vowel_harmony.js
@@ -1,5 +1,17 @@
 const isHasMajorVowelHarmony = (word) => {
-    return false;
-}
+  const hardVowels = ["a", "ı", "o", "u"];
+  const softVowels = ["e", "i", "ö", "ü"];
+
+  let hardCounter = 0;
+  let softCounter = 0;
+
+  [...word].forEach((char) => {
+    if ([...hardVowels, ...softVowels].includes(char) === false) return false; // letter is not vowel, just skip this one.
+    if (hardVowels.includes(char)) return hardCounter++;
+    if (softVowels.includes(char)) return softCounter++;
+  });
+
+  return !(hardCounter > 0 && softCounter > 0);
+};
 
 export default isHasMajorVowelHarmony;
